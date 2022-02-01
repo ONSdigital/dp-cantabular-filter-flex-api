53f8521e5e91ec84e9a0d03e94b6c8f7b6d2c4cf
Joshua Medley <jmedleyx7@gmail.com>
2022-02-01 16:37:24 +0000

    add tests for request body validation

--- api/contract_test.go
+++ api/contract_test.go
@@ -1,41 +1,103 @@
 package api
 
 import (
+	"testing"
+
+	"github.com/ONSdigital/dp-cantabular-filter-flex-api/model"
+
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/google/uuid"
 )
 
-func testUUID() uuid.UUID{
-	const idstr := "307c53db-4495-436f-8f2e-8435deb8144e"
+func testUUID() *uuid.UUID{
+	const idstr = "307c53db-4495-436f-8f2e-8435deb8144e"
 	id, err := uuid.Parse(idstr)
 	So(err, ShouldBeNil)
 
-	return id
+	return &id
 }
 
 func TestCreateFiltersRequestValid(t *testing.T){
 	Convey("Given a valid createFilterRequest request object", t, func() {
 		req := createFilterRequest{
-			InstanceID: 
-			DatasetID      string            `bson:"dataset_id"      json:"dataset_id"`
-			Edition        string            `bson:"edition"         json:"edition"`
-			Version        int               `bson:"version"         json:"version"`
-			CantabularBlob string            `bson:"cantabular_blob" json:"cantabular_blob"`
-			Dimensions     []model.Dimension `bson:"dimensions"      json:"dimensions"`
+			InstanceID:     testUUID(),
+			DatasetID:      "test-dataset-id",
+			Edition:        "test-edition",
+			Version:        1,
+			CantabularBlob: "test-blob",
+			Dimensions:     []model.Dimension{
+				{
+					Name: "test-dimension-1",
+					Options: []string{"a", "b", "c"},
+					DimensionURL: "http://dim-1.com",
+					IsAreaType: true,
+				},
+				{
+					Name: "test-dimension-2",
+					Options: []string{"1", "2", "3"},
+					DimensionURL: "http://dim-2.com",
+					IsAreaType: false,
+				},
+			},
 		}
 
-		Convey("When ParseRequest(body, request) is called", func() {
-			err := api.ParseRequest(bytes.NewReader(b), &req)
+		Convey("When Valid() is called", func() {
+			err := req.Valid()
 			So(err, ShouldBeNil)
+		})
 
-			Convey("The request object should be populated with the expected values ", func() {
-				expected := fooRequest{
-					Foo:   "I am foo",
-					Bar:   2,
-					IsValid: true,
-				}
-				So(req, ShouldResemble, expected)
+		Convey("Given instanceID is missing", func() {
+			r := req
+			r.InstanceID = nil
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("Given datasetID is missing", func() {
+			r := req
+			r.DatasetID = ""
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("Given version is 0/missing", func() {
+			r := req
+			r.Version = 0
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("Given edition is missing", func() {
+			r := req
+			r.Edition = ""
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("If there are less than 2 dimensions", func() {
+			r := req
+			r.Dimensions = r.Dimensions[:1]
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
+			})
+		})
+
+		Convey("Given a dimension is invalid", func() {
+			r := req
+			r.Dimensions[0] = model.Dimension{}
+			Convey("When Valid() is called", func() {
+				err := r.Valid()
+				So(err, ShouldNotBeNil)
 			})
 		})
 	})
 }
-
