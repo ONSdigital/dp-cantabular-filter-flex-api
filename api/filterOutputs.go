b51893eeb3fc612facc5635e8840d595d5b51e5c
jitendar.rawat@methods.co.uk <jitendar.rawat@methods.co.uk>
2022-02-25 09:52:12 +0000

    Fixed minor issues

--- api/filterOutputs.go
+++ api/filterOutputs.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func (api *API) createFilterOutputs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello!!")
 }
--- datastore/mongodb/filterOutputs.go
+++ datastore/mongodb/filterOutputs.go
@@ -2,41 +2,14 @@ package mongodb
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/model"
-
-	"github.com/pkg/errors"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 // CreateFilter creates a new Filter in the CantabularFilters colllection
-func (c *Client) CreateFilterOutputs(ctx context.Context, f *model.Filter) error {
-	var err error
+func (c *Client) CreateFilterOutputs(ctx context.Context, f *model.FilterOutput) error {
+	//var err error
 	//****this is just a copy of CreateFilter, need to update this *****//
-	if f.ID, err = c.generate.UUID(); err != nil {
-		return errors.Wrap(err, "failed to generate UUID: %w")
-	}
-
-	if f.ETag, err = f.Hash(nil); err != nil {
-		return errors.Wrap(err, "failed to generate eTag: %w")
-	}
-
-	f.Links.Self = model.Link{
-		HREF: fmt.Sprintf("%s/flex/filters/%s", c.cfg.FilterFlexAPIURL, f.ID),
-	}
-
-	col := c.collections.filters
-
-	lockID, err := col.lock(ctx, f.ID.String())
-	if err != nil {
-		return err
-	}
-	defer col.unlock(ctx, lockID)
-
-	if _, err = c.conn.Collection(col.name).UpsertById(ctx, f.ID, bson.M{"$set": f}); err != nil {
-		return errors.Wrap(err, "failed to upsert filter")
-	}
 
 	return nil
 }
