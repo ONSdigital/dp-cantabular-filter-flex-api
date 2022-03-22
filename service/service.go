a0cff1ca1597ae354c9ccb34e185e9c3b8dd9c75
saminahbab <saminahbab0@gmail.com>
2022-03-22 16:23:50 +0000

    feat: init kafka producer

--- service/service.go
+++ service/service.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/api"
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/config"
+	kafka "github.com/ONSdigital/dp-kafka/v3"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/identity"
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
@@ -24,6 +25,7 @@ type Service struct {
 	Api              *api.API
 	responder        Responder
 	store            Datastore
+	Producer         kafka.IProducer
 	generator        Generator
 	cantabularClient CantabularClient
 	datasetAPIClient DatasetAPIClient
@@ -49,6 +51,11 @@ func (svc *Service) Init(ctx context.Context, cfg *config.Config, buildTime, git
 	// Get HealthCheck
 	if svc.HealthCheck, err = GetHealthCheck(cfg, buildTime, gitCommit, version); err != nil {
 		return fmt.Errorf("could not instantiate healthcheck: %w", err)
+	}
+
+	if svc.Producer, err = GetKafkaProducer(ctx, cfg); err != nil {
+		return fmt.Errorf("Could not initialise Kafka producer: %w", err)
+
 	}
 
 	svc.cantabularClient = GetCantabularClient(cfg)
