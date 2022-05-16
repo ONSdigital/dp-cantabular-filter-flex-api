c4fe03722057c48605450b477f9bf59250dfdd41
jitendar.rawat@methods.co.uk <jitendar.rawat@methods.co.uk>
2022-05-16 13:43:56 +0100

    dimensionIds is now an array of strings

--- schema/schema.go
+++ schema/schema.go
@@ -13,7 +13,7 @@ var exportStart = `{
     {"name": "edition",          "type": "string", "default": ""},
     {"name": "version",          "type": "string", "default": ""},
     {"name": "filter_output_id", "type": "string", "default": ""},
-    {"name": "dimension_ids",    "type": { "type": "array", "items": "string"}, "default": [] }
+    {"name": "dimensions",       "type": { "type": "array", "items": "string"}, "default": [] }
   ]
 }`
 
