d8b357b9646b021084c22e735ba68efeff785c82
PatrickConnollyONS <Patrick.Connolly@ext.ons.gov.uk>
2022-08-31 16:59:28 +0100

    Enhance the Cantabular Client for the mocker Cantabular server with extra functionality

--- features/mock/cantabular_client.go
+++ features/mock/cantabular_client.go
@@ -9,9 +9,11 @@ import (
 )
 
 type CantabularClient struct {
-	ErrStatus            int
-	OptionsHappy         bool
-	SearchDimensionsFunc func(ctx context.Context, req cantabular.SearchDimensionsRequest) (*cantabular.GetDimensionsResponse, error)
+	ErrStatus                  int
+	OptionsHappy               bool
+	SearchDimensionsFunc       func(ctx context.Context, req cantabular.SearchDimensionsRequest) (*cantabular.GetDimensionsResponse, error)
+	GetGeographyDimensionsFunc func(context.Context, cantabular.GetGeographyDimensionsRequest) (*cantabular.GetGeographyDimensionsResponse, error)
+	StaticDatasetQueryFunc     func(context.Context, cantabular.StaticDatasetQueryRequest) (*cantabular.StaticDatasetQuery, error)
 }
 
 func (c *CantabularClient) Reset() {
@@ -30,12 +32,18 @@ func (c *CantabularClient) GetDimensionOptions(_ context.Context, _ cantabular.G
 	return nil, errors.New("invalid dimension options")
 }
 
-func (c *CantabularClient) StaticDatasetQuery(context.Context, cantabular.StaticDatasetQueryRequest) (*cantabular.StaticDatasetQuery, error) {
-	return nil, errors.New("invalid dataset query")
+func (c *CantabularClient) StaticDatasetQuery(ctx context.Context, req cantabular.StaticDatasetQueryRequest) (*cantabular.StaticDatasetQuery, error) {
+	if c.OptionsHappy {
+		return c.StaticDatasetQueryFunc(ctx, req)
+	}
+	return nil, errors.New("error while executing dataset query")
 }
 
-func (c *CantabularClient) GetGeographyDimensions(context.Context, cantabular.GetGeographyDimensionsRequest) (*cantabular.GetGeographyDimensionsResponse, error) {
-	return nil, errors.New("invalid geography query")
+func (c *CantabularClient) GetGeographyDimensions(ctx context.Context, req cantabular.GetGeographyDimensionsRequest) (*cantabular.GetGeographyDimensionsResponse, error) {
+	if c.OptionsHappy {
+		return c.GetGeographyDimensionsFunc(ctx, req)
+	}
+	return nil, errors.New("error while getting geography dimensions")
 }
 
 func (c *CantabularClient) SearchDimensions(ctx context.Context, req cantabular.SearchDimensionsRequest) (*cantabular.GetDimensionsResponse, error) {
