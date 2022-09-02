efc5e1d13b51ea3f16a77e173505fa1f1ef9a92f
PatrickConnollyONS <Patrick.Connolly@ext.ons.gov.uk>
2022-09-02 09:05:45 +0100

    Enhance the Cantabular and Dataset Features with the extra steps needed for the geography tests, allowing a mocked cantabular server which speaks GraphQL to be used.

--- features/steps/cantabularfeature.go
+++ features/steps/cantabularfeature.go
@@ -3,38 +3,78 @@ package steps
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strings"
+	"testing"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabular"
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabular/gql"
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/config"
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/features/mock"
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/service"
+	dphttp "github.com/ONSdigital/dp-net/v2/http"
 
 	"github.com/cucumber/godog"
 )
 
 type CantabularFeature struct {
-	*mock.CantabularClient
+	cantabularMock   *mock.CantabularClient
+	cantabularServer *mock.CantabularServer
+
+	cfg *config.Config
 }
 
-func NewCantabularFeature() *CantabularFeature {
-	return &CantabularFeature{CantabularClient: &mock.CantabularClient{OptionsHappy: true}}
+func NewCantabularFeature(t *testing.T, cfg *config.Config) *CantabularFeature {
+	return &CantabularFeature{
+		cantabularMock:   &mock.CantabularClient{OptionsHappy: true},
+		cantabularServer: mock.NewCantabularServer(t),
+		cfg:              cfg,
+	}
 }
 
 func (cf *CantabularFeature) Reset() {
-	cf.CantabularClient.Reset()
+	cf.cantabularMock.Reset()
+	cf.cantabularServer.Reset()
+
+	cf.setMockedInterface()
 }
 
 func (cf *CantabularFeature) RegisterSteps(ctx *godog.ScenarioContext) {
+	ctx.Step(
+		`^the Cantabular service is a mocked extended Cantabular server`,
+		cf.useAMockedExtCantabularServer,
+	)
+	ctx.Step(
+		`^the Cantabular service is a mocked interface$`,
+		cf.setMockedInterface,
+	)
+
 	ctx.Step(
 		`^Cantabular returns these dimensions for the dataset "([^"]*)" and search term "([^"]*)":$`,
 		cf.cantabularSearchReturnsTheseDimensions,
 	)
 	ctx.Step(
+		`^Cantabular returns these geography dimensions for the given request:$`,
+		cf.cantabularReturnsTheseGeographyDimensionsForTheGivenRequest,
+	)
+	ctx.Step(
+		`^Cantabular returns this static dataset for the given request:$`,
+		cf.cantabularReturnsThisStaticDatasetForTheGivenRequest,
+	)
+
+	ctx.Step(
 		`^Cantabular responds with an error$`,
 		cf.cantabularRespondsWithAnError,
 	)
+}
+
+func (cf *CantabularFeature) useAMockedExtCantabularServer() error {
+	cf.cfg.CantabularExtURL = cf.cantabularServer.ResolveURL("")
+
+	cf.setMockedServer()
+
+	return nil
 }
 
 func (cf *CantabularFeature) cantabularSearchReturnsTheseDimensions(datasetID, dimension string, docs *godog.DocString) error {
@@ -43,7 +83,7 @@ func (cf *CantabularFeature) cantabularSearchReturnsTheseDimensions(datasetID, d
 		return fmt.Errorf("unable to unmarshal cantabular search response: %w", err)
 	}
 
-	cf.CantabularClient.SearchDimensionsFunc = func(ctx context.Context, req cantabular.SearchDimensionsRequest) (*cantabular.GetDimensionsResponse, error) {
+	cf.cantabularMock.SearchDimensionsFunc = func(ctx context.Context, req cantabular.SearchDimensionsRequest) (*cantabular.GetDimensionsResponse, error) {
 		if req.Dataset == datasetID && req.Text == dimension {
 			return &response, nil
 		}
@@ -62,12 +102,54 @@ func (cf *CantabularFeature) cantabularSearchReturnsTheseDimensions(datasetID, d
 	return nil
 }
 
+func (cf *CantabularFeature) cantabularReturnsTheseGeographyDimensionsForTheGivenRequest(docs *godog.DocString) error {
+	request, response, found := strings.Cut(docs.Content, "response:")
+	if !found {
+		return errors.New("CantabularFeature::cantabularReturnsTheseGeographyDimensionsForTheGivenRequest - request and response were not found")
+	}
+	request = strings.TrimPrefix(request, "request:")
+
+	cf.cantabularServer.Handle([]byte(request), []byte(response))
+
+	return nil
+}
+
+func (cf *CantabularFeature) cantabularReturnsThisStaticDatasetForTheGivenRequest(docs *godog.DocString) error {
+	request, response, found := strings.Cut(docs.Content, "response:")
+	if !found {
+		return errors.New("CantabularFeature::cantabularReturnsThisStaticDatasetForTheGivenRequest - request and response were not found")
+	}
+	request = strings.TrimPrefix(request, "request:")
+
+	cf.cantabularServer.Handle([]byte(request), []byte(response))
+
+	return nil
+}
+
 func (cf *CantabularFeature) cantabularRespondsWithAnError() {
-	cf.OptionsHappy = false
+	cf.cantabularMock.OptionsHappy = false
+}
+
+func (cf *CantabularFeature) setMockedServer() {
+	service.GetCantabularClient = func(cfg *config.Config) service.CantabularClient {
+		return cantabular.NewClient(
+			cantabular.Config{
+				Host:           cfg.CantabularURL,
+				ExtApiHost:     cfg.CantabularExtURL,
+				GraphQLTimeout: cfg.DefaultRequestTimeout,
+			},
+			dphttp.NewClient(),
+			nil,
+		)
+	}
+}
+
+func (cf *CantabularFeature) setMockedInterface() {
+	service.GetCantabularClient = func(cfg *config.Config) service.CantabularClient {
+		return cf.cantabularMock
+	}
 }
 
 func (cf *CantabularFeature) setInitialiserMock() {
-	service.GetCantabularClient = func(cfg *config.Config) service.CantabularClient {
-		return cf.CantabularClient
-	}
+	cf.setMockedInterface()
 }
