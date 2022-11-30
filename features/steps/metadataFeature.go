8d0dab3439d29b53b70b37098e294b570d5669ae
jitendar <jitendar.rawat@methods.co.uk>
2022-11-30 15:28:51 +0000

    Fixed issues around mock

--- features/steps/metadataFeature.go
+++ features/steps/metadataFeature.go
@@ -1,27 +1,22 @@
 package steps
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabularmetadata"
+	api "github.com/ONSdigital/dp-cantabular-filter-flex-api/api/mock"
 	"github.com/ONSdigital/dp-cantabular-filter-flex-api/config"
-	"github.com/ONSdigital/dp-cantabular-filter-flex-api/features/mock"
 	"github.com/cucumber/godog"
 )
 
 type MetadataFeature struct {
-	*mock.MetadataApiClient
+	*api.MockmetadataAPIClient
 }
 
 func NewMetadataFeature(t *testing.T, mfg *config.Config) *MetadataFeature {
-	return &MetadataFeature{MetadataApiClient: &mock.MetadataApiClient{OptionsHappy: true}}
-}
-
-func (mf *MetadataFeature) Reset() {
-	mf.MetadataApiClient.Reset()
+	return &MetadataFeature{MockmetadataAPIClient: &api.MockmetadataAPIClient{}}
 }
 
 func (mf *MetadataFeature) RegisterSteps(ctx *godog.ScenarioContext) {
@@ -43,14 +38,10 @@ func (mf *MetadataFeature) MetadataReturnsTheseDefaults(input *godog.DocString) 
 		return fmt.Errorf("unable to unmarshal cantabular search response: %w", err)
 	}
 
-	mf.MetadataApiClient.GetDefaultCategorisationFunc = func(ctx context.Context, req cantabularmetadata.GetDefaultClassificationRequest) (*cantabularmetadata.GetDefaultClassificationResponse, error) {
-		return cantabularResponse, nil
-	}
-
 	return nil
 }
 
 func (mf *MetadataFeature) MetadataReturnsAnError() error {
-	mf.OptionsHappy = false
+	//	mf.OptionsHappy = false
 	return nil
 }
